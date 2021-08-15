f29d5da11772bc337e27c8799524b619c6888b95
RomanovRoma <romanian.reget@gmail.com>
2021-08-15 19:13:07 +0300

    refactor: refactor code

--- src/components/pokemon-list-item/pokemon-list-item.js
+++ src/components/pokemon-list-item/pokemon-list-item.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import './pokemon-list-item.css'
+import React from "react";
+import "./pokemon-list-item.css";
 
 const PokemonListItem = ({ pokemon }) => {
   const { id, name } = pokemon;
   const numToStr = String(id);
   let newId;
   if (numToStr.length === 1) {
-    newId =  '00' + numToStr;
+    newId = "00" + numToStr;
   } else if (numToStr.length === 2) {
-    newId =  '0' + numToStr;
+    newId = "0" + numToStr;
   } else {
     newId = numToStr;
   }
-    return (
-      <div className="pokemon-list-item">
-        <div className="pokemon-img">
-          <img
-            className="pokemon-image"
-            src={`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${newId}.png`}
-            alt={name}
-          />
-        </div>
-        <div className="pokemon-details">
-          <div className="pokemon-name">
-            <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
-          </div>
+  return (
+    <div className="pokemon-list-item">
+      <div className="pokemon-img">
+        <img
+          className="pokemon-image"
+          src={`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${newId}.png`}
+          alt={name}
+        />
+      </div>
+      <div className="pokemon-details">
+        <div className="pokemon-name">
+          <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default PokemonListItem
+export default PokemonListItem;
